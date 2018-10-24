603f906abeaffbe7ae750035afb1f5d66eb083a4
NathanPri <prigent.nathan@gmail.com>
2018-10-24 15:16:30 +0200

    Footer a jour

--- 13-pizzastore/index.php
+++ 13-pizzastore/index.php
@@ -5,12 +5,7 @@ require_once(__DIR__.'/partials/header.php'); ?>
     <main role="main" class="container">
         <h1>Mon Pizza Store WebSite</h1>
     </main>
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="assets/js/script.js"></script>
-  </body>
-</html>
+
+<?php
+// Le fichier footer.php est inclus sur la page
+require_once(__DIR__.'/partials/footer.php'); ?>
