49445edba1a4ee432b37c0c55d830973e39225cd
NathanPri <prigent.nathan@gmail.com>
2018-10-24 15:44:42 +0200

    ombre:active

--- 13-pizzastore/partials/header.php
+++ 13-pizzastore/partials/header.php
@@ -17,20 +17,24 @@
   </head>
 
   <body>
+  <?php
+    //   Si request_uri vaut /home/toto/fichier.php $page renverra fichier 
+        $page = basename($_SERVER['REQUEST_URI'],'.php'); ?>
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="index.php">Pizza Store</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-pizzastore">
         <span class="navbar-toggler-icon"></span>
       </button>
+       
 
       <div class="collapse navbar-collapse" id="navbar-pizzastore">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Accueil</a>
+          <li class="nav-item <?= ($page === 'index') ? 'active' : ''; ?>">
+            <a class="nav-link" href="index.php">Accueil</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Liste des Pizzas</a>
+          <li class="nav-item <?= ($page === 'pizza_list') ? 'active' : ''; ?>">
+            <a class="nav-link" href="pizza_list.php">Liste des Pizzas</a>
           </li>
         </ul>
       </div>
